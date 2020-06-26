a90683d851a92e4d36c771d290e20b501ebe9e4e
saumya786 <saumyakundu786@gmail.com>
2020-06-26 21:56:40 +0530

    Converted The Web Page into PWA

---
+++ README.md
@@ -0,0 +1,4 @@
+# First Test Repository
+This Repository is mainly made to check whether PWA is working on github pages.
+
+This application mainly converts time in one time zone to another.
---
+++ sw.js
@@ -0,0 +1,45 @@
+const staticCacheName= 'site-static-v1';
+const assets = [
+    '/',
+    '/index.html',
+    '/dateTimeConvertor.css',
+    '/finalDateFunctions.js',
+    '/app.js'
+];
+//install service worker
+self.addEventListener('install',evt=>{
+    //console.log('Service worker has been installed');
+    evt.waitUntil(
+        caches.open(staticCacheName)
+        .then(cache => {
+            console.log('Caching Shell Assets');
+            cache.addAll(assets);
+        })
+    );
+});
+//Activate Event
+self.addEventListener('activate',evt=>{
+    console.log('Service worker has been activated');
+    evt.waitUntil(
+        //This a
+        caches.keys().then(
+            keys=>{
+                //console.log(keys);
+                return Promise.all(keys
+                    .filter(key => key!==staticCacheName)
+                    .map(key => caches.delete(key))
+                )
+            }
+        )
+    );
+});
+
+//Fetch Event
+self.addEventListener('fetch',evt=>{
+    //console.log('Fetch Event',evt);
+    evt.respondWith(
+        caches.match(evt.request).then(cacheRes => {
+            return cacheRes || fetch(evt.request);
+        })
+    );
+});
